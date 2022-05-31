8bb19d9301d8c88f27ee43256da769f7aec57c66
Victor Perez <Zunaty@gmail.com>
2022-05-31 13:39:44 -0600

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controllers');
+
+// /api/user/
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/user/:id
+router.route('/:id')
+    .get(getOneUSer)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
